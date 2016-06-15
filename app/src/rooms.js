6f15a1812ed4951facbb880c7aca064b493d36e5
David <goodwid@gmail.com>
2016-06-14 21:53:44 -0700

    Added a longer hallway

--- app/src/rooms.js
+++ app/src/rooms.js
@@ -9,19 +9,23 @@ class Room {
     if (this[dir] === null) {
       response.msg = 'Sorry, you cannot go that way';
     } else {
-      response.msg = `You travel ${dir} to ${this[dir].title}`;
+      response.msg = `You travel ${dir} to the ${this[dir].title}\n`;
+      response.msg += this[dir].desc;
       if (this[dir].obj.length > 0) response.obj = this[dir].obj;
     }
     return response;
   }
   look () {
-    return {title: this.title, msg: this.desc, obj: this.obj}
+    let response = ''
+    response += `You are in the ${this.title}.  ${this.desc}\n`;
+    if (this.obj.length > 0) this.obj.forEach(item => response += (`There is a ${item} on the floor.\n`));
+    return response;
   }
 }
 
-const room1 = new Room({
-  title: 'Room 1',
-  desc: 'large room',
+const study = new Room({
+  title: 'study',
+  desc: 'There is a desk here.  There is a door to the north.\n',
   obj: ['bag'],
   n: null,
   e: null,
@@ -29,9 +33,9 @@ const room1 = new Room({
   s: null
 });
 
-const room2 = new Room({
-  title: 'Room 2',
-  desc: 'smaller room',
+const den = new Room({
+  title: 'den',
+  desc: 'Bookshelves line the room.  There is a door to the south.\n',
   obj: ['key'],
   n: null,
   e: null,
@@ -39,9 +43,27 @@ const room2 = new Room({
   s: null
 });
 
-const hallway = new Room({
+const hallwayNorth = new Room({
   title: 'hallway',
-  desc: 'a long hallway',
+  desc: 'You are in a long hallway.  There is a door to the north and the hallway continues south.\n',
+  obj: [],
+  n: null,
+  e: null,
+  w: null,
+  s: null
+});
+const hallwayMiddle = new Room({
+  title: 'hallway',
+  desc: 'You are in a long hallway.  It continues to the north and south.\n',
+  obj: [],
+  n: null,
+  e: null,
+  w: null,
+  s: null
+});
+const hallwaySouth = new Room({
+  title: 'hallway',
+  desc: 'You are in a long hallway.  There is a door to the south and the hallway continues north.\n',
   obj: [],
   n: null,
   e: null,
@@ -50,26 +72,33 @@ const hallway = new Room({
 });
 
 function buildMap() {
-  room1.n = hallway;
-  room2.s = hallway;
-  hallway.s = room1;
-  hallway.n = room2;
+  study.n = hallwaySouth;
+  den.s = hallwayNorth;
+  hallwayNorth.s = hallwayMiddle;
+  hallwayNorth.n = den;
+  hallwayMiddle.n = hallwayNorth;
+  hallwayMiddle.s = hallwaySouth;
+  hallwaySouth.n = hallwayMiddle;
+  hallwaySouth.s = study;
 }
 
 
 user = {
-  location: room1,
+  location: study,
   inventory: [],
   go (dir) {
+    let response = {};
     let result = this.location.travel(dir);
     if (result.room) this.location = result.room;
-    console.log (result.msg);
-    if (result.obj) result.obj.forEach(item => console.log (`There is a ${item} on the floor.`));
+    response.msg = result.msg;
+    if (result.obj) result.obj.forEach(item => response.msg += (`There is a ${item} on the floor.\n`));
+    return response;
   }
 }
 
 function command(c) {
-  cmd = c.toLowerCase().trim().split(' ');
+  let response = {};
+  let cmd = c.toLowerCase().trim().split(' ');
   switch (cmd[0]) {
     case 'north':
     case 'south':
@@ -79,34 +108,42 @@ function command(c) {
     case 'e':
     case 's':
     case 'w': {
-      user.go(cmd[0][0]);
+      return user.go(cmd[0][0]);
       break;
     }
     case 'look':
     case 'l': {
-      console.log(user.location.look());
+      response.msg = user.location.look();
       break;
     }
     case 'get': {
       let itemIndex = user.location.obj.indexOf(cmd[1]);
       if (itemIndex > -1) {
         item = user.location.obj[itemIndex];
-        console.log(`You picked up a ${item}.`);
+        response.msg = `You picked up a ${item}.`;
         user.inventory.push(item);
         user.location.obj.splice(itemIndex,1);
-      } else console.log(`There is no ${cmd[1] || 'item'} here to get`);
+      } else response.msg = `There is no ${cmd[1] || 'item'} here to get`;
       break;
     }
     case 'drop': {
+      if (cmd[1] === 'all' && user.inventory.length > 0) {
+        user.inventory.forEach(item => user.location.obj.push(item));
+        user.inventory = [];
+        response.msg = 'You have dropped all items.';
+        break;
+      }
       let itemIndex = user.inventory.indexOf(cmd[1])
       if ( itemIndex > -1) {
         item = user.inventory[itemIndex];
-        console.log(`You have dropped a ${item}`);
+        response.msg = `You have dropped a ${item}`;
         user.location.obj.push(item);
         user.inventory.splice(itemIndex,1);
-      } else console.log('You do not have that item');
+      } else response.msg = 'You do not have that item';
       break;
     }
+    case 'inv':
+    case 'inventory':
     case 'i': {
       if (user.inventory !== []) {
         console.log('You have the following items:');
@@ -117,6 +154,7 @@ function command(c) {
     case 'quit':
     case 'q': process.exit(0);
   }
+  return response;
 }
 
 
@@ -124,7 +162,8 @@ function prompt() {
   process.stdout.write('> ');
 }
 process.stdin.on('data', text =>  {
-  command(text.toString());
+  result = command(text.toString());
+  console.log(result.msg);
   prompt();
 });
 
