02d2e476d169929283a2cdfde30a32aa8806530b
Johnny Luangphasy <johnny@jluangphasy.com>
2016-06-15 08:57:40 -0700

    Cli UI working in browser

---
+++ app/src/cli.js
@@ -0,0 +1,14 @@
+const commands = {
+  n: 'travel north',
+  e: 'travel east',
+  s: 'travel south',
+  w: 'travel west',
+  get: 'get an item',
+  drop: 'drop an item',
+  help: 'list of commands',
+  inventory: 'list of items collected',
+  clear: 'clear the cli screen',
+  look: 'show description'
+};
+
+export { commands as default };
